b2687a217012f989607aa05137e318badca6aefa
euna501@gmail.com <euna501@gmail.com@fffd7567-2d10-7787-fbd7-507c15c80025>
2014-03-17 13:25:58 +0000

    update test

--- trunk/test/src/com/example/test/MainActivity.java
+++ trunk/test/src/com/example/test/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+    Button a = new Button;
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
