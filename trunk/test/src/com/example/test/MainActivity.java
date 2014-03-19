3dd229a8bc03285f86cc1db5131f057c8389ad57
ninehappy7@gmail.com <ninehappy7@gmail.com@fffd7567-2d10-7787-fbd7-507c15c80025>
2014-03-19 09:28:40 +0000

    test1

--- trunk/test/src/com/example/test/MainActivity.java
+++ trunk/test/src/com/example/test/MainActivity.java
@@ -23,6 +23,8 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+        
+        int a;
     }
 
 
