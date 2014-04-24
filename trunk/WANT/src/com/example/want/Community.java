c013786eb00d06a92420c6e6829d9ae44ea4d975
euna501@gmail.com <euna501@gmail.com@fffd7567-2d10-7787-fbd7-507c15c80025>
2014-04-24 16:21:48 +0000

    커뮤니티 학년별 메뉴 Intent추가

--- trunk/WANT/src/com/example/want/Community.java
+++ trunk/WANT/src/com/example/want/Community.java
@@ -1,8 +1,12 @@
 package com.example.want;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
 
 public class Community extends ActionBarActivity {
@@ -17,6 +21,60 @@ public class Community extends ActionBarActivity {
 		ActionBar actionBar = getSupportActionBar();
 		actionBar.hide();
 	
+		ImageButton homeButton = (ImageButton)findViewById(R.id.homeButton);
+		homeButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+				startActivity(intent);
+			}
+		});
+		
+		ImageButton firstgraderButton = (ImageButton)findViewById(R.id.firstgraderButton);
+		firstgraderButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(getApplicationContext(), Firstgrader_Notice.class);
+				startActivity(intent);
+			}
+		});
+		
+		ImageButton secondgraderButton = (ImageButton)findViewById(R.id.secondgraderButton);
+		secondgraderButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(getApplicationContext(), Secondgrader_Notice.class);
+				startActivity(intent);
+			}
+		});
+		
+		ImageButton thirdgraderButton = (ImageButton)findViewById(R.id.thirdgraderButton);
+		thirdgraderButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(getApplicationContext(), Thirdgrader_Notice.class);
+				startActivity(intent);
+			}
+		});
+		
+		ImageButton fourthgraderButton = (ImageButton)findViewById(R.id.fourthgraderButton);
+		fourthgraderButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(getApplicationContext(), Fourthgrader_Notice.class);
+				startActivity(intent);
+			}
+		});
 	}
 
 }
