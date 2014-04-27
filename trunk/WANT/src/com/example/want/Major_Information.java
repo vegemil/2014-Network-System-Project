d737e6bbd77e6fdcdf27ebc9acd4c5a3b9d9b86d
ninehappy7@gmail.com <ninehappy7@gmail.com@fffd7567-2d10-7787-fbd7-507c15c80025>
2014-04-27 08:12:27 +0000

    학과정보 -> 학과소개,전공, 과실시간표등등 -> 액티비티 전환

--- trunk/WANT/src/com/example/want/Major_Information.java
+++ trunk/WANT/src/com/example/want/Major_Information.java
@@ -6,16 +6,29 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ListView;
 
 public class Major_Information extends ActionBarActivity {
-
+	 static final String[] inforlist = {"학과소개", "교수님소개", "전공", "학과공지사항" , "과실시간표"} ;
+	 private ArrayAdapter<String> arrayAdapter ;
+	 private ListView listView ;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.major_information);
-		
+	
+		 arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,  inforlist);
+
+	        listView =  (ListView)findViewById(R.id.listView1);
+	        listView.setAdapter(arrayAdapter);
+	        listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+	        listView.setOnItemClickListener(onItemClickListener);
+	      	
 		// 액션바 숨김
 		ActionBar actionBar = getSupportActionBar();
 		actionBar.hide();
@@ -29,7 +42,42 @@ public class Major_Information extends ActionBarActivity {
 				Intent intent = new Intent(getApplicationContext(), MainActivity.class);
 				startActivity(intent);
 			}
-		});
+		});	
+	}
+	
+	private OnItemClickListener onItemClickListener = new AdapterView.OnItemClickListener()
+
+    {
+
+        @Override
+
+        public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+
+        {
+        	if(position ==0){
+        		Intent intent = new Intent(Major_Information.this, Major_Introduction.class);
+        		startActivity(intent);
+        	}
+        	if(position ==1){
+        		Intent intent = new Intent(Major_Information.this, Professor_Introduction.class);
+        		startActivity(intent);
+        	}
+        	if(position ==2){
+        		Intent intent = new Intent(Major_Information.this, Major.class);
+        		startActivity(intent);
+        	}
+        	if(position ==3){
+        		Intent intent = new Intent(Major_Information.this, Major_Notice.class);
+        		startActivity(intent);
+        	}
+        	if(position ==4){
+        		Intent intent = new Intent(Major_Information.this, Timetable.class);
+        		startActivity(intent);
+        	}
+
+        }
+
+    };	
+		
 	}
 
-}
