cc6c4c57ddb61f5e872d675b9a816fffe7d01e9e
euna501@gmail.com <euna501@gmail.com@fffd7567-2d10-7787-fbd7-507c15c80025>
2014-04-30 19:06:12 +0000

    과실 시간표 UI작업 완료 Spinner작업중 에러 확인

--- trunk/WANT/src/com/example/want/Timetable.java
+++ trunk/WANT/src/com/example/want/Timetable.java
@@ -6,7 +6,12 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Timetable extends ActionBarActivity {
 
@@ -30,6 +35,20 @@ public class Timetable extends ActionBarActivity {
 				startActivity(intent);
 			}
 		});
+		//스피너 에러중
+//		Spinner classSpinner = (Spinner)findViewById(R.id.classSpinner);
+//		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.class_array, R.layout.support_simple_spinner_dropdown_item);
+//		adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+//		classSpinner.setAdapter(adapter);
+//		classSpinner.setOnItemClickListener(new OnItemClickListener() {
+//
+//			@Override
+//			public void onItemClick(AdapterView<?> parent, View view,
+//					int position, long id) {
+//				// TODO Auto-generated method stub
+//				TextView selectText = (TextView)findViewById(R.id.classTextView);
+//				
+//			}
+//		});
 	}
-
 }
