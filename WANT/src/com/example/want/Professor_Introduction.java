8ddb9a5a5c16fe1490f4cec820707ece1f265db2
92ppppp@gmail.com <92ppppp@gmail.com@fffd7567-2d10-7787-fbd7-507c15c80025>
2014-05-07 14:08:41 +0000

    교수님소개부분추가 !

--- WANT/src/com/example/want/Professor_Introduction.java
+++ WANT/src/com/example/want/Professor_Introduction.java
@@ -7,6 +7,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class Professor_Introduction extends ActionBarActivity {
 
@@ -30,6 +31,20 @@ public class Professor_Introduction extends ActionBarActivity {
 				startActivity(intent);
 			}
 		});
+		
+		TextView kim = (TextView) findViewById(R.id.kim);
+		TextView jung = (TextView) findViewById(R.id.jung);
+		TextView lim = (TextView) findViewById(R.id.lim);
+		
+		kim.setText("성명 : 김학수\r\n직위 : 교수\r\n연구실 : 정보과학관 6320호\r\n전화번호 : 02-2610-4347\r\n이메일 : hskim@skhu.ac.kr"
+				+ " \r\n학력사항 : 연세대학교 전자공학과\r\n관심분야 : 영상통신, 영상압축,\r\n                  영상처리");
+		
+		jung.setText("성명 : 정연식\r\n직위 : 부교수\r\n연구실 : 정보과학관 6314호\r\n전화번호 : 02-2610-4726\r\n이메일 : ysjeong@skhu.ac.kr"
+				+ " \r\n학력사항 : 연세대학교 전자공학과\r\n관심분야 : Wireless Networks and\r\n                  Mobile Computing");
+		
+		lim.setText("성명 : 임충규\r\n직위 : 조교수\r\n연구실 : 정보과학관 6308호\r\n전화번호 : 02-2610-4313\r\n이메일 : cglim@skhu.ac.kr"
+				+ " \r\n학력사항 : Louisiana State\r\n                  University\r\n                  Computer Science\r\n관심분야 : 컴퓨터 그래픽스\r\n                  컴퓨터 게임, 기하 모델링");
+		
 	}
 
 }
