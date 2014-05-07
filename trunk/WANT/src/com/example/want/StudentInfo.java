0b291e0da90b37c7f62e727f27fc8eba14ea1176
euna501@gmail.com <euna501@gmail.com@fffd7567-2d10-7787-fbd7-507c15c80025>
2014-05-07 19:31:46 +0000

    회원가입 데이터 받는 것 확인

---
+++ trunk/WANT/src/com/example/want/StudentInfo.java
@@ -0,0 +1,40 @@
+package com.example.want;
+
+public class StudentInfo {
+	private int id;
+	private int password;
+	private int grade;
+	private String name;
+
+	public void setID(int inputID) {
+		id = inputID;
+	}
+
+	public int getID() {
+		return id;
+	}
+
+	public void setPassword(int inputPassword) {
+		password = inputPassword;
+	}
+
+	public int getPassword() {
+		return password;
+	}
+
+	public void setGrade(int inputgrade) {
+		grade = inputgrade;
+	}
+
+	public int getGrade() {
+		return grade;
+	}
+
+	public void setName(String inputName) {
+		name = inputName;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
