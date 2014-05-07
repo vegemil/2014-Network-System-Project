51aa1864d031d66692c6d0a26ac12d65db15d4ac
euna501@gmail.com <euna501@gmail.com@fffd7567-2d10-7787-fbd7-507c15c80025>
2014-05-07 13:24:28 +0000

    커뮤니티 UI 완성

---
+++ trunk/WANT/src/com/example/want/Secondgrader_comment.java
@@ -0,0 +1,19 @@
+package com.example.want;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+
+public class Secondgrader_comment extends ActionBarActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.secondgrader_comment);
+		
+		// ¾×¼Ç¹Ù ¼û±è
+				ActionBar actionBar = getSupportActionBar();
+				actionBar.hide();
+	}
+
+}
