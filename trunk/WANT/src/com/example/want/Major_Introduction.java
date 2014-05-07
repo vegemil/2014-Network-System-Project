6f405a7ed44fd6cef6ec4992e0b67c70695c2f81
92ppppp@gmail.com <92ppppp@gmail.com@fffd7567-2d10-7787-fbd7-507c15c80025>
2014-05-07 16:40:30 +0000

    학과안내 자바 커밋이용

--- trunk/WANT/src/com/example/want/Major_Introduction.java
+++ trunk/WANT/src/com/example/want/Major_Introduction.java
@@ -7,6 +7,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class Major_Introduction extends ActionBarActivity {
 
@@ -30,6 +31,14 @@ public class Major_Introduction extends ActionBarActivity {
 				startActivity(intent);
 			}
 		});
+		
+		TextView majorintro = (TextView) findViewById(R.id.majorintro);
+		
+		majorintro.setText("우리 학과는 사람들이\r\n편리하게 생활할 수\r\n있도록, 그로 인해\r\n삶의 질을 더 높일\r\n수 있도록 소중한 인재를\r\n"
+				+ "양성하는데 교육의 목표를 두고 있습니다.\r\n\r\n프로그래밍 언어, 네트워크, 컴퓨터그래픽스, 게임프로그래밍, 멀티미디어처리 등의 다양한 교과과정을"
+				+ " 학생들에게 제공하고 있으며 학생들의 융합적인 사고와 실무 능력을 배양하기 위한 프로젝트 수업 및 회사를 미리 경험해볼 수 있는 현장연수 수업도 제공하고 있습니다."
+				+ "\r\n\r\n우리는 미래 지향적인 인재들을 만들기 위해 노력하고 있습니다.\r\n\r\n여러분 도전하십시오.");
+		
 	}
 
 }
