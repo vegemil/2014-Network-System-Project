c1813049e31a25bcb4d33b6cc37fcc7cf5255527
euna501@gmail.com <euna501@gmail.com@fffd7567-2d10-7787-fbd7-507c15c80025>
2014-05-09 19:16:00 +0000

    TCP/IP 전송 확인 DB랑 비교하는거 추가 예정

--- trunk/JoinServer/src/JoinServer.java
+++ trunk/JoinServer/src/JoinServer.java
@@ -1,69 +1,98 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-public class JoinServer extends Thread {
-	public static final int SERVERPORT = 4444;
-	private PrintWriter Out;
+public class JoinServer {
 
-	public void sendMessage(String message) {
-		if (Out != null && !Out.checkError()) {
-			Out.println(message);
-			Out.flush();
-		}
-	}
+	public static void main(String[] args) throws IOException {
+		ServerSocket serverSocket = null;
+		Socket clientSocket = null;
+		PrintWriter out = null;
+		BufferedReader in = null;
 
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		super.run();
+		serverSocket = new ServerSocket(5555);
 
 		try {
-			System.out.println("S : Connecting");
+			clientSocket = serverSocket.accept();
+			System.out.println("클라이언트 연결");
 
-			ServerSocket serverSocket = new ServerSocket(SERVERPORT);
+			out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(
+					clientSocket.getOutputStream(), "UTF-8")), true);
+			in = new BufferedReader(new InputStreamReader(
+					clientSocket.getInputStream(), "UTF-8"));
 
-			Socket client = serverSocket.accept();
-			System.out.println("S : Receving");
+			String name;
+			String grade;
+			String id;
+			String password;
 
-			try {
-				// 스트링 전송
-				Out = new PrintWriter(new BufferedWriter(
-						new OutputStreamWriter(client.getOutputStream())), true);
+			name = in.readLine();
+			grade = in.readLine();
+			id = in.readLine();
+			password = in.readLine();
 
-				// 스트링 받음
-				BufferedReader in = new BufferedReader(new InputStreamReader(
-						client.getInputStream()));
+			System.out.println(name);
+			System.out.println(grade);
+			System.out.println(id);
+			System.out.println(password);
 
-				String name = in.readLine();
-				String grade = in.readLine();
-				String id = in.readLine();
-				String password = in.readLine();
-				
-				System.out.println("name : " + name);
-				System.out.println("grade : " + grade);
-				System.out.println("id : " + id);
-				System.out.println("password : "+ password);
+			out.println("회원가입 성공");
+			out.flush();
 
-			} catch (Exception e) {
-				// TODO: handle exception
-				System.out.println("S : Error");
-				e.printStackTrace();
-			}
-			finally{
-				client.close();
-				System.out.println("S : Done");
-			}
-
+			out.close();
+			in.close();
+			clientSocket.close();
+			serverSocket.close();
 		} catch (Exception e) {
 			// TODO: handle exception
-			System.out.println("S : Error");
 			e.printStackTrace();
 		}
 	}
 
+	public void getStudentInfoFromDB(String id) {
+		try {
+			Connection con = null;
+
+			con = DriverManager
+					.getConnection(
+							"jdbc:mysql://network.cgdc8rvnyyii.ap-northeast-1.rds.amazonaws.com:3306",
+							"want", "32102637");
+
+			Statement st = null;
+			ResultSet rs = null;
+			st = con.createStatement();
+			rs = st.executeQuery("USE network");
+
+			// 테이블리스트 출력 쿼리 전송
+			if (st.execute("SHOW TABLES")) {
+
+				rs = st.getResultSet();
+			}
+
+			while (rs.next()) {
+
+				String str = rs.getNString(1);
+
+				// rs에서 해당 필드의 값을 가져와 출력
+				System.out.println(str);
+
+			}
+
+		} catch (SQLException sqex) {
+
+			System.out.println("SQLException: " + sqex.getMessage());
+			System.out.println("SQLState: " + sqex.getSQLState());
+
+		}
+	}
 }
