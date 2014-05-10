b8a33a5674b04941f197d1c9011aa5eb6983ea2f
euna501@gmail.com <euna501@gmail.com@fffd7567-2d10-7787-fbd7-507c15c80025>
2014-05-10 17:07:18 +0000

    로그인 코드 작성중 tcp 확인 아직 안함

--- WANT/src/com/example/want/Login.java
+++ WANT/src/com/example/want/Login.java
@@ -1,15 +1,65 @@
 package com.example.want;
 
+import com.example.want.Join.connectTask;
+
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
-public class Login extends ActionBarActivity {
+public class Login extends ActionBarActivity implements AsyncResponse{
 
+		String name;
+		String grade;
+		String id;
+		String password;
+
+		static String serverMessage;
+		static String serverName;
+		static String serverGrade;
+
+		private TCPClient myTcpClient;
+		private StudentInfo myStudent;
+
+		public class connectTask extends AsyncTask<String, String, String> {
+			public AsyncResponse delegate = null;
+
+			@Override
+			protected String doInBackground(String... message) {
+
+				// we create a TCPClient object and
+				myTcpClient = new TCPClient(new TCPClient.OnMessageReceived() {
+
+					@Override
+					// here the messageReceived method is implemented
+					public void messageReceived(String message) {
+						// this method calls the onProgressUpdate
+						publishProgress(message);
+
+						//serverMessage = message;
+					}
+				});
+				myTcpClient.run();
+
+				return serverMessage;
+			}
+
+			@Override
+			protected void onProgressUpdate(String... values) {
+				super.onProgressUpdate(values);
+				serverMessage = values[0];
+				serverName = values[1];
+				serverGrade = values[2];
+			}
+
+		}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -42,6 +92,50 @@ public class Login extends ActionBarActivity {
 				startActivity(intent);
 			}
 		});
+		
+		final EditText idEdit = (EditText)findViewById(R.id.loginidedit);
+		final EditText passwordEdit = (EditText)findViewById(R.id.loginpasswordedit);
+		
+		final connectTask connect = new connectTask();
+		connect.execute("");
+		connect.delegate = this;
+		
+		ImageButton okButton = (ImageButton) findViewById(R.id.loginokButton);
+		okButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				id = idEdit.getText().toString();
+				password = passwordEdit.getText().toString();
+				
+				Log.i("tag", "id : " + id);
+				Log.i("tag", "password : " + password);
+				
+				if (myTcpClient != null) {
+					myTcpClient.sendMessage(id);
+					myTcpClient.sendMessage(password);
+				}
+				
+				while (serverMessage == null || serverMessage.isEmpty()) {
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				
+				Log.i("tag", "name : " + name);
+				Log.i("tag", "grade : " + grade );
+			}
+		});
 	}
 
+	@Override
+	public void processFinish(String output) {
+		// TODO Auto-generated method stub
+		serverMessage = output;
+		Log.i("tag", "processFinish result : " + serverMessage);
+	}
 }
