1d5cb4db8900c7907a2cea9c3d840688c67bd256
euna501@gmail.com <euna501@gmail.com@fffd7567-2d10-7787-fbd7-507c15c80025>
2014-05-11 06:49:18 +0000

    로그인 서버 완료 소켓 한개만 받음ㅋㅋㅋㅋ

--- trunk/LoginServer/src/LoginServer.java
+++ trunk/LoginServer/src/LoginServer.java
@@ -6,11 +6,15 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class LoginServer {
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		ServerSocket serverSocket = null;
 		Socket clientSocket = null;
@@ -18,49 +22,165 @@ public class LoginServer {
 		BufferedReader in = null;
 
 		String result = null;
+		String name = null;
+		String grade = null;
 
-		serverSocket = new ServerSocket(5555);
+		serverSocket = new ServerSocket(6666);
 
 		try {
 
-				clientSocket = serverSocket.accept();
-				System.out.println("클라이언트 연결");
+			clientSocket = serverSocket.accept();
+			System.out.println("클라이언트 연결");
 
-				try {
-					out = new PrintWriter(new BufferedWriter(
-							new OutputStreamWriter(
-									clientSocket.getOutputStream(), "UTF-8")),
-							true);
-					in = new BufferedReader(new InputStreamReader(
-							clientSocket.getInputStream(), "UTF-8"));
+			try {
+				out = new PrintWriter(new BufferedWriter(
+						new OutputStreamWriter(clientSocket.getOutputStream(),
+								"UTF-8")), true);
+				in = new BufferedReader(new InputStreamReader(
+						clientSocket.getInputStream(), "UTF-8"));
 
-					String id;
-					String password;
+				String id;
+				String password;
 
-					id = in.readLine();
-					password = in.readLine();
+				id = in.readLine();
+				password = in.readLine();
 
-					System.out.println(id);
-					System.out.println(password);
+				System.out.println(id);
+				System.out.println(password);
 
-					
-					System.out.println("result : " + result);
-					out.println(result);
-					out.flush();
-				} catch (Exception e) {
-					// TODO: handle exception
-					e.printStackTrace();
-				} 
-				out.close();
-				in.close();
-				clientSocket.close();
-				serverSocket.close();
+				result = findID(id);
+				System.out.println("result : " + result);
+				out.println(result);
+
+				if (result.equals("비밀번호 확인하러 고고")) {
+					name = getName(id);
+					grade = getGrade(id);
+					out.println(name);
+					out.println(grade);
+				}
+
+				out.flush();
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+			out.close();
+			in.close();
+			clientSocket.close();
+			serverSocket.close();
 
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
 		}
 	}
-		
 
+	public static String findID(String id) {
+
+		String result = null;
+
+		try {
+			Connection con = null;
+
+			con = DriverManager
+					.getConnection(
+							"jdbc:mysql://network.cgdc8rvnyyii.ap-northeast-1.rds.amazonaws.com:3306?useUnicode=true&characterEncoding=euckr",
+							"want", "32102637");
+
+			Statement st = null;
+			ResultSet rs = null;
+			st = con.createStatement();
+			rs = st.executeQuery("USE network");
+
+			// 테이블리스트 출력 쿼리 전송
+			if (st.execute("SELECT * FROM studentdb WHERE id= " + id)) {
+				rs = st.getResultSet();
+			}
+
+			if (rs.next()) {
+				result = "비밀번호 확인하러 고고";
+			} else {
+				result = "없는 아이디 입니다.";
+			}
+
+		} catch (SQLException sqex) {
+
+			System.out.println("SQLException: " + sqex.getMessage());
+			System.out.println("SQLState: " + sqex.getSQLState());
+
+		}
+		return result;
+	}
+
+	public static String getName(String id) {
+		String result = null;
+
+		try {
+			Connection con = null;
+
+			con = DriverManager
+					.getConnection(
+							"jdbc:mysql://network.cgdc8rvnyyii.ap-northeast-1.rds.amazonaws.com:3306?useUnicode=true&characterEncoding=euckr",
+							"want", "32102637");
+
+			Statement st = null;
+			ResultSet rs = null;
+			st = con.createStatement();
+			rs = st.executeQuery("USE network");
+
+			// 테이블리스트 출력 쿼리 전송
+			if (st.execute("SELECT * FROM studentdb WHERE id= " + id)) {
+				rs = st.getResultSet();
+			}
+
+			if (rs.next()) {
+				result = rs.getString("name");
+			} else {
+				result = "이름을 가져 올 수 없습니다.";
+			}
+
+		} catch (SQLException sqex) {
+
+			System.out.println("SQLException: " + sqex.getMessage());
+			System.out.println("SQLState: " + sqex.getSQLState());
+
+		}
+
+		return result;
+
+	}
+
+	public static String getGrade(String id) {
+		String result = null;
+
+		try {
+			Connection con = null;
+
+			con = DriverManager
+					.getConnection(
+							"jdbc:mysql://network.cgdc8rvnyyii.ap-northeast-1.rds.amazonaws.com:3306?useUnicode=true&characterEncoding=euckr",
+							"want", "32102637");
+
+			Statement st = null;
+			ResultSet rs = null;
+			st = con.createStatement();
+			rs = st.executeQuery("USE network");
+
+			// 테이블리스트 출력 쿼리 전송
+			if (st.execute("SELECT * FROM studentdb WHERE id= " + id)) {
+				rs = st.getResultSet();
+			}
+
+			if (rs.next()) {
+				result = rs.getString("grade");
+			} else {
+				result = "학년을 가져올 수 없습니다.";
+			}
+
+		} catch (SQLException sqex) {
+			System.out.println("SQLException: " + sqex.getMessage());
+			System.out.println("SQLState: " + sqex.getSQLState());
+		}
+		return result;
+	}
 }
