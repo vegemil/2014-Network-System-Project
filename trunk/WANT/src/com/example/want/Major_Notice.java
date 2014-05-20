ae7474adb78e23a7318f174b5a956c01da9338fb
ninehappy7@gmail.com <ninehappy7@gmail.com@fffd7567-2d10-7787-fbd7-507c15c80025>
2014-05-20 12:01:46 +0000

    가져오기 버튼없이 바로 파싱목록 보여주기 파싱내용 공백좀 제거 했음!

--- trunk/WANT/src/com/example/want/Major_Notice.java
+++ trunk/WANT/src/com/example/want/Major_Notice.java
@@ -9,28 +9,23 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.TextView;
 
 
 
 public class Major_Notice extends ActionBarActivity {
 	
-	private ArrayList<HashMap<String, String>> data;
-	private Button bt_open;
+	private ArrayList<HashMap<String, String>> data;	
 	private ListView list;
 	private SimpleAdapter sa;
 	private Major_Notice2 hp;
-	private TextView tv;
-	//private ArrayAdapter<String> arrayAdapter;
+		
 	private final Handler handler = new Handler()
 	{
 		@Override
@@ -44,20 +39,15 @@ public class Major_Notice extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.major_notice);//Major_Notice.java는 major_notice랑 영결됨 ㅇㅋㅌ?ㅇㅋ
+		setContentView(R.layout.major_notice);
 			
-			tv = (TextView)findViewById(R.id.tv_major);
-		    data = new ArrayList<HashMap<String, String>>();
-	        
-	        bt_open = (Button)findViewById(R.id.bt_open);
-	        list = (ListView)findViewById(R.id.listView1);
-	        
-	        hp = new Major_Notice2(Major_Notice.this, handler, data);
-	        
+		    data = new ArrayList<HashMap<String, String>>();	        
+	        list = (ListView)findViewById(R.id.listView1);	        
+	        hp = new Major_Notice2(Major_Notice.this, handler, data);	        
 	        sa = new SimpleAdapter(Major_Notice.this, data, R.layout.major_notice2,
 	        		new String[]{"title","writer","day","test","text"}, new int[]{R.id.tv_title, R.id.tv_writer, R.id.tv_day,R.id.tv_test,R.id.tv_major});
 	        
-	        bt_open.setOnClickListener(listener);
+	        
 	        list.setAdapter(sa);
 	        
 	        list.setOnItemClickListener(new OnItemClickListener() {
@@ -70,9 +60,8 @@ public class Major_Notice extends ActionBarActivity {
 	        		HashMap<String, String> item = (HashMap<String, String>)parent.getItemAtPosition(position);
 	        		Intent it = new Intent(Major_Notice.this, majortext.class);
 	        		it.putExtra("text", item.get("text"));
-	        		//Log.d("null 검사","누가널이니"+item.get("text"+"") );
-	        		//tv.setText(item.get("text"+""));
 	        		startActivity(it);
+	        		
 					
 				}
 			});
@@ -92,19 +81,10 @@ public class Major_Notice extends ActionBarActivity {
 				startActivity(intent);
 			}
 		});
+		hp.open();
 	}
 	
 	
-	 OnClickListener listener = new OnClickListener(){
-
-			@Override
-			public void onClick(View arg0)
-			{
-				hp.open();
-			}
-	    };
-	    
-	    
 	  //업데이트하기
 	    private void listUpdate()
 	    {
