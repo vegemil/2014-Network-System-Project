488d885db292e4ca287445e273622058c0c35165
92ppppp@gmail.com <92ppppp@gmail.com@fffd7567-2d10-7787-fbd7-507c15c80025>
2014-05-20 04:15:01 +0000

    NFC태그ID를 서버로 전송하는 부분 !
    서버를 키면 바로 "서버"라는 문자 출력, 클라이언트와 연결되면 바로 "클라이언트연결", "연결된 디바이스IP" 출력
    NFC스티커에 태깅을 하게되면 바로 "TAGID"출력 (한번만해도바로출력됨)

--- trunk/WANT/src/com/example/want/MainActivity.java
+++ trunk/WANT/src/com/example/want/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.want;
 
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,14 +17,60 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
+	private NfcAdapter nfcAdapter;
+	private PendingIntent pendingIntent;
+	static String[] serverMessage = new String[3];
+	static int count = 0;
+	
+	private TCPClient myTcpClient;
+	//StudentInfo myStudentInfo;
 
+	public class connectTask extends AsyncTask<String, String, String> {
+		public MainActivity delegate = null;
+
+		@Override
+		protected String doInBackground(String... message) {
+
+			// we create a TCPClient object and
+			myTcpClient = new TCPClient(new TCPClient.OnMessageReceived() {
+
+				@Override
+				// here the messageReceived method is implemented
+				public void messageReceived(String message) {
+					// this method calls the onProgressUpdate
+					publishProgress(message);
+					serverMessage[count] = message;
+					count++;
+					
+					message = serverMessage[0];
+					Log.i("Tag ID", "서버에서 받은 값 : " + message);
+				}
+			}, 7777);
+			myTcpClient.run();
+			return null;
+		}
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.activity_main);
-
+		
+		 nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+	     Intent intent = new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+	     pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+	     Log.e("화면 : " , "화면이 켜졌습니다.");
+	     
+	     Log.i("서버에 접속중 ㅎㅎㅎ","");
+			final connectTask connect = new connectTask();
+			connect.execute("");
+			
+			connect.delegate = this;
+			
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
@@ -125,5 +176,61 @@ public class MainActivity extends ActionBarActivity {
 			return rootView;
 		}
 	}
+	protected void onPause() {
+		if (nfcAdapter != null) {
+			nfcAdapter.disableForegroundDispatch(this);
+		}
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		if (nfcAdapter != null) {
+			nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null);
+		}
+	}
+
+	@Override
+	protected void onNewIntent(Intent intent) {
+		super.onNewIntent(intent);
+		
+		Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+		if (tag != null) {
+			byte[] tagId = tag.getId();
+			
+			serverMessage[0] = toHexString(tagId); // tagid서버로 전송		 
+			
+			Toast.makeText(getApplicationContext(), "태그되었습니다.", Toast.LENGTH_SHORT).show();
+			
+			if (myTcpClient != null) {
+				myTcpClient.sendMessage(serverMessage[0]);
+			}
+			
+			
+			while (serverMessage[0] == null || serverMessage[0].isEmpty()) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		}
+	}
+		
+	
+	public static final String CHARS = "0123456789ABCDEF";
+	
+	public static String toHexString(byte[] data) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < data.length; ++i) {
+			sb.append(CHARS.charAt((data[i] >> 4) & 0x0F))
+				.append(CHARS.charAt(data[i] & 0x0F));
+		}
+		return sb.toString();
+	}
 
 }
+
