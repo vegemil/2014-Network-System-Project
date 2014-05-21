6c9293a9f81f9a8eec1bb4a715cb0c8f5ca48201
euna501@gmail.com <euna501@gmail.com@fffd7567-2d10-7787-fbd7-507c15c80025>
2014-05-21 16:59:05 +0000

    커뮤니티 글 내용까지 출력

--- WANT/src/com/example/want/Secondgrader_Notice.java
+++ WANT/src/com/example/want/Secondgrader_Notice.java
@@ -10,6 +10,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -21,6 +23,8 @@ public class Secondgrader_Notice extends ActionBarActivity implements
 	static String[] title = new String[100];
 	static String[] date = new String[100];
 	static String[] writer = new String[100];
+	static String[] context = new String[100];
+	static String[] textnum = new String[100];
 	static int listCount = 0;
 	static int count = 0;
 	
@@ -63,8 +67,6 @@ public class Secondgrader_Notice extends ActionBarActivity implements
 		protected void onProgressUpdate(String... values) {
 			super.onProgressUpdate(values);
 
-			Toast.makeText(getApplicationContext(), "로딩중입니다.",
-					Toast.LENGTH_SHORT).show();
 		}
 
 	}
@@ -119,11 +121,8 @@ public class Secondgrader_Notice extends ActionBarActivity implements
 			}
 		}
 		
-		
 		myTcpClient.sendMessage("2");
-		
-		
-		
+				
 		while (serverMessage[0] == null || serverMessage[0].isEmpty()) {
 			try {
 				Thread.sleep(1000);
@@ -140,11 +139,15 @@ public class Secondgrader_Notice extends ActionBarActivity implements
 		 arrData = new ArrayList<Community_List_Data>();
 		 
 		 for (int i = 0; i < listCount; i++) {
-		 writer[i] = serverMessage[3 * i + 1];
-		 title[i] = serverMessage[3 * i + 2];
-		 date[i] = serverMessage[3 * (i + 1)];
+		 writer[i] = serverMessage[5 * i + 1];
+		 title[i] = serverMessage[5 * i + 2];
+		 date[i] = serverMessage[5 * i + 3];
+		 context[i] = serverMessage[5 * i +4];
+		 textnum[i] = serverMessage[5 * (i + 1)];
 		
 		 Log.i("onCreate 결과", i +" : " + writer[i] + ", " + title[i] + ", " + date[i]);
+		 Log.i("onCreate 결과", context[i]);
+		 Log.i("onCreate 결과","" + textnum[i]);
 		 arrData.add(new Community_List_Data(title[i], writer[i], date[i]));
 		 }
 		 		
@@ -152,6 +155,30 @@ public class Secondgrader_Notice extends ActionBarActivity implements
 		 list = (ListView)findViewById(R.id.listView1);
 		 list.setAdapter(adapter);
 		 
+		 list.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// TODO Auto-generated method stub
+				Community_Text_Data.setContext(context[position]);
+				Community_Text_Data.setTitle(title[position]);
+				Community_Text_Data.setTextNum(textnum[position]);
+				Community_Text_Data.setDate(date[position]);
+				Community_Text_Data.setWriter(writer[position]);
+				
+				Log.i("Community text data", Community_Text_Data.getContext());
+				Log.i("Community text data", Community_Text_Data.getDate());
+				Log.i("Community text data", Community_Text_Data.getTextNum());
+				Log.i("Community text data", Community_Text_Data.getTitle());
+				Log.i("Community text data", Community_Text_Data.getWriter());
+				Intent intent = new Intent(getApplicationContext(),
+						Secondgrader_NoticeBody.class);
+				startActivity(intent);
+			}
+		});
+		 
+		 myTcpClient.stopClient();
 	}
 	
 	
