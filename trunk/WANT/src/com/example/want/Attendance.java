9acad096b8d823004766c7fef7f465612d96b03c
0517tjsal@gmail.com <0517tjsal@gmail.com@fffd7567-2d10-7787-fbd7-507c15c80025>
2014-05-22 11:39:38 +0000

    출석버튼누르고 태그하기

--- trunk/WANT/src/com/example/want/Attendance.java
+++ trunk/WANT/src/com/example/want/Attendance.java
@@ -1,9 +1,18 @@
 package com.example.want;
 
+import com.example.want.MainActivity.PlaceholderFragment;
+import com.example.want.Attendance.connectTask;
+
 import android.R.string;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
@@ -11,14 +20,138 @@ import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Attendance extends ActionBarActivity {
 
+	private NfcAdapter nfcAdapter;
+	private PendingIntent pendingIntent;
+	static String[] serverMessage = new String[3];
+	static int count = 0;
+	private TCPClient myTcpClient;
+
+    
+	
+	
+	public class connectTask extends AsyncTask<String, String, String> {
+		public Attendance delegate = null;
+
+		@Override
+		protected String doInBackground(String... message) {
+
+			// we create a TCPClient object and
+			myTcpClient = new TCPClient(new TCPClient.OnMessageReceived() {
+			
+				@Override
+				// here the messageReceived method is implemented
+				public void messageReceived(String message) {
+					// this method calls the onProgressUpdate
+					publishProgress(message);
+					serverMessage[count] = message;
+					count++;
+					
+					message = serverMessage[0];
+					Log.i("Tag ID", "서버에서 받은 값 : " + message);
+				}
+			}, 3333);
+			myTcpClient.run();
+			return null;
+		}
+	}
+	    
+	
+	 
+	
+	    protected void onPause() {
+			if (nfcAdapter != null) {
+				nfcAdapter.disableForegroundDispatch(this);
+			}
+			super.onPause();
+		}
+
+		@Override
+		protected void onResume() {
+			super.onResume();
+			if (nfcAdapter != null) {
+				nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null);
+			}
+		}
+
+		@Override
+		protected void onNewIntent(Intent intent) {
+			super.onNewIntent(intent);
+			
+			Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+			if (tag != null) {
+				byte[] tagId = tag.getId();
+				
+				serverMessage[0] = toHexString(tagId); // tagid서버로 전송		 
+				
+				Toast.makeText(getApplicationContext(), "태그되었습니다.", Toast.LENGTH_SHORT).show();
+				
+				if (myTcpClient != null) {
+					myTcpClient.sendMessage(serverMessage[0]);
+				}
+				
+				
+				while (serverMessage[0] == null || serverMessage[0].isEmpty()) {
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				
+			}
+		}
+			
+		
+		
+		
+			
+		
+		
+		public static final String CHARS = "0123456789ABCDEF";
+		
+		public static String toHexString(byte[] data) {
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < data.length; ++i) {
+				sb.append(CHARS.charAt((data[i] >> 4) & 0x0F))
+					.append(CHARS.charAt(data[i] & 0x0F));
+			}
+			return sb.toString();
+		}
+	
+	
+		
+		
+		
+		
+		
+		
+		
+		
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.attendance);
+		nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+		Intent intent = new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+		pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+		final connectTask connect = new connectTask();
+		connect.execute("");
+		
+		connect.delegate = this;
+		
+	/*if (savedInstanceState == null) {
+		getSupportFragmentManager().beginTransaction()
+				.add(R.id.container, new PlaceholderFragment()).commit();
+	}*/
 
+
+		
 		// 액션바 숨김
 		ActionBar actionBar = getSupportActionBar();
 		actionBar.hide();
