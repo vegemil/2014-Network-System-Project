10894c1ee2398c6c1a1e4900a136166ba0bf8447
euna501@gmail.com <euna501@gmail.com@fffd7567-2d10-7787-fbd7-507c15c80025>
2014-05-29 16:36:49 +0000

    커뮤니티 글쓰기 보낼 데이터 만듬

--- trunk/WANT/src/com/example/want/Write_Secondgrade.java
+++ trunk/WANT/src/com/example/want/Write_Secondgrade.java
@@ -1,11 +1,16 @@
 package com.example.want;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class Write_Secondgrade extends ActionBarActivity {
@@ -29,6 +34,32 @@ public class Write_Secondgrade extends ActionBarActivity {
 				Intent intent = new Intent(getApplicationContext(),
 						MainActivity.class);
 				startActivity(intent);
+			}
+		});
+		
+		ImageButton okButton = (ImageButton)findViewById(R.id.okButton);
+		okButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				EditText titleEdit =(EditText)findViewById(R.id.titleEdit);
+				EditText bodyEdit = (EditText)findViewById(R.id.bodyEdit);
+				
+				String title = titleEdit.getText().toString();
+				String body = bodyEdit.getText().toString();
+				String name = StudentInfo.getName();
+				String id = StudentInfo.getID();
+				
+				// 현재 시간을 msec으로 구한다.
+				long now = System.currentTimeMillis();
+				// 현재 시간을 저장 한다.
+				Date date = new Date(now);
+				// 시간 포맷으로 만든다.
+				SimpleDateFormat sdfNow = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREA);
+				String strNow = sdfNow.format(date);
+				
+				
 			}
 		});
 	}
