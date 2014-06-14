41abeaf8ff14250df068a4a5932ea50a03b03ffa
92ppppp@gmail.com <92ppppp@gmail.com@fffd7567-2d10-7787-fbd7-507c15c80025>
2014-06-14 05:44:40 +0000

    주차별 데이버 구분하게끔 NFCReadServer변경

--- NFCReadServer/src/NFCReadServer.java
+++ NFCReadServer/src/NFCReadServer.java
@@ -16,10 +16,18 @@ import java.sql.Statement;
 
 public class NFCReadServer {
 
-	static String[] id = new String[100];
-	static String[] first_check = new String[100];
+	static String[] id = new String[500];
+	static String[] first_check = new String[500];
+	static String[] second_check = new String[500];
+	static String[] third_check = new String[500];
 
 	static int count = 0;
+	static int count2 = 0;
+	static int count3 = 0;
+	
+	
+	
+	static String[] week = new String[3];
 
 	public static void main(String[] args) throws IOException, SQLException {
 		ServerSocket serverSocket = null;
@@ -49,15 +57,31 @@ public class NFCReadServer {
 
 					out.println(count);
 					System.out.println(count);
-
+					
 					for (int i = 0; i < count; i++) {
 						out.println(id[i]);
 						out.println(first_check[i]);
 					}
 
+					
+					out.println(count2);
+					for (int i = 0; i < count2; i++) {
+						out.println(id[i]);
+						out.println(second_check[i]);
+					}
+					
+					out.println(count3);
+					for (int i = 0; i < count3; i++) {
+						out.println(id[i]);
+						out.println(third_check[i]);
+					}
+							
+
 					out.flush();
 
 					count = 0;
+					count2 = 0;
+					count3 = 0;
 
 				} catch (Exception e) {
 					// TODO: handle exception
@@ -86,33 +110,89 @@ public class NFCReadServer {
 
 		try {
 			Statement st = null;
+			Statement st2 = null;
 			ResultSet rs = null;
+			ResultSet rs2 = null;
+			
+			Statement st3 = null;
+			ResultSet rs3 = null;
 			st = con.createStatement();
 			rs = st.executeQuery("USE network");
-
+			st2=con.createStatement();
+			rs2 = st2.executeQuery("USE network");
+			st3=con.createStatement();
+			rs3 = st3.executeQuery("USE network");
 			// 테이블리스트출력쿼리전송
+			/*
 			if (st.execute("SELECT id, time FROM subject_network")) {
 				rs = st.getResultSet();
-			}
-
-			while (rs.next()) {
-				id[count] = rs.getNString("id");
-				first_check[count] = rs.getTimestamp("time").toString(); // 1주차의시간을계산해
+			}*/
+			
+					
+		
+			if (st.execute("SELECT id, time FROM subject_network where date_format(time, '%Y-%m-%d') = '2014-06-11'")) {
+				rs = st.getResultSet();				
+				while (rs.next()) {
+					id[count] = rs.getNString("id");
+					first_check[count] = rs.getTimestamp("time").toString(); // 1주차의시간을계산해
 																			// time이일주차인것만
 																			// 긁어와야함
-				count++;
+					count++;
+				}
 			}
+			
+			//week[0] = "1st";
+			
+			
+			if (st2.execute("SELECT id, time FROM subject_network where date_format(time, '%Y-%m-%d') = '2014-06-12'")) {
+				rs2 = st2.getResultSet();						
+				while (rs2.next()) {
+					id[count2] = rs2.getNString("id");
+					second_check[count2] = rs2.getTimestamp("time").toString(); // 2주차의시간을계산해
+																				// time이일주차인것만
+																			// 긁어와야함
+					count2++;				
+				}
+			}
+			
+
+			if (st3.execute("SELECT id, time FROM subject_network where date_format(time, '%Y-%m-%d') = '2014-06-13'")) {
+				rs3 = st3.getResultSet();						
+				while (rs3.next()) {
+					id[count3] = rs3.getNString("id");
+					third_check[count3] = rs3.getTimestamp("time").toString(); // 2주차의시간을계산해
+																				// time이일주차인것만
+																			// 긁어와야함
+					count3++;				
+				}
+			}
+			
+			
+			
 
 			for (int i = 0; i < count; i++) {
 				System.out.println("id : " + id[i]);
-				System.out.println("first_check : " + first_check[i]);
+				System.out.println("first_check : " + first_check[i]);				
 				System.out.println("-------------------");
 
 			}
+			for(int b=0;b< count2;b++){
+				System.out.println("id : " + id[b]);
+				System.out.println("second_check : " + second_check[b]);
+			}
+			
+			for (int c = 0; c < count3; c++) {
+				System.out.println("id : " + id[c]);
+				System.out.println("third_check : " + third_check[c]);				
+				System.out.println("-------------------");
+
+			}
+			
 
 		} catch (SQLException sqex) {
 			System.out.println("SQLException: " + sqex.getMessage());
 			System.out.println("SQLState: " + sqex.getSQLState());
+			System.out.println("-------------------");
 		}
 
 	}
