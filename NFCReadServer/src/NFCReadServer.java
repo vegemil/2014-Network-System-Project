f1f90982e1f0ce4cd93a36cef45f06de877c001e
ninehappy7@gmail.com <ninehappy7@gmail.com@fffd7567-2d10-7787-fbd7-507c15c80025>
2014-06-14 20:06:13 +0000

    id 안뜨는거 다뜨게했뚜!

--- NFCReadServer/src/NFCReadServer.java
+++ NFCReadServer/src/NFCReadServer.java
@@ -4,11 +4,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.io.ObjectInputStream.GetField;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -17,6 +15,9 @@ import java.sql.Statement;
 public class NFCReadServer {
 
 	static String[] id = new String[500];
+	static String[] id2 = new String[500];
+	static String[] id3 = new String[500];
+	
 	static String[] first_check = new String[500];
 	static String[] second_check = new String[500];
 	static String[] third_check = new String[500];
@@ -65,13 +66,13 @@ public class NFCReadServer {
 					
 					out.println(count2);
 					for (int i = 0; i < count2; i++) {
-						out.println(id[i]);
+						out.println(id2[i]);
 						out.println(second_check[i]);
 					}
 					
 					out.println(count3);
 					for (int i = 0; i < count3; i++) {
-						out.println(id[i]);
+						out.println(id3[i]);
 						out.println(third_check[i]);
 					}							
 
@@ -147,7 +148,7 @@ public class NFCReadServer {
 			if (st2.execute("SELECT id, time FROM subject_network where date_format(time, '%Y-%m-%d') = '2014-06-12'")) {
 				rs2 = st2.getResultSet();						
 				while (rs2.next()) {
-					id[count2] = rs2.getNString("id");
+					id2[count2] = rs2.getNString("id");
 					second_check[count2] = rs2.getTimestamp("time").toString(); // 2주차의시간을계산해
 																				// time이일주차인것만
 																			// 긁어와야함
@@ -159,7 +160,7 @@ public class NFCReadServer {
 			if (st3.execute("SELECT id, time FROM subject_network where date_format(time, '%Y-%m-%d') = '2014-06-13'")) {
 				rs3 = st3.getResultSet();						
 				while (rs3.next()) {
-					id[count3] = rs3.getNString("id");
+					id3[count3] = rs3.getNString("id");
 					third_check[count3] = rs3.getTimestamp("time").toString(); // 3주차의시간을계산해
 																				// time이일주차인것만
 																			// 긁어와야함
